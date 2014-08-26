0b974db79300f520c114ac46405c1c1b962b433d
Akbar Gumbira <akbargumbira@gmail.com>
2014-08-26 16:47:08 +0700

    Add admin page for the model.

--- user_map/admin.py
+++ user_map/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+# coding=utf-8
+"""Model Admin Class."""
 
-# Register your models here.
+from django.contrib.gis import admin
+
+from user_map.models import User, Role
+
+
+class UserAdmin(admin.GeoModelAdmin):
+    """Admin Class for User Model."""
+    list_display = ('name', 'email', 'role', 'website', 'email_updates',
+                    'last_login')
+    list_filter = ['role', 'is_approved', 'is_admin']
+    search_fields = ['name', 'email']
+    fieldsets = [
+        ('Basic Information', {
+            'fields': [
+                'name', 'email', 'website', 'role', 'email_updates']}),
+        ('Location', {'fields': ['location']}),
+        ('Advanced Information', {
+            'fields': ['is_approved', 'is_active', 'is_admin', 'last_login']}),
+    ]
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Role)
