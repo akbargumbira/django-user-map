6e1c8f90f6ce60770883344d23584efc949f15cd
Akbar Gumbira <akbargumbira@gmail.com>
2016-02-25 22:56:01 +0100

    Add admin site to get the namespace for login view template for testing.

--- user_map/tests/urls.py
+++ user_map/tests/urls.py
@@ -1,4 +1,6 @@
+# coding=utf-8
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 urlpatterns = patterns(
     '',
@@ -6,6 +8,6 @@ urlpatterns = patterns(
     url(r'^login/$',
         'django.contrib.auth.views.login',
         {'template_name': 'admin/login.html'},
-        name='my_login',
-    ),
+        name='my_login'),
+    url(r'^admin/', include(admin.site.urls)),
 )
