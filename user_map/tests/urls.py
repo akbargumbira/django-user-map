1599bc03b0a1cd202836479fba2406457a17f118
Akbar Gumbira <akbargumbira@gmail.com>
2016-02-25 22:29:17 +0100

    Add login url for testing.

--- user_map/tests/urls.py
+++ user_map/tests/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'^user-map/', include('user_map.urls', namespace='user_map'))
+    url(r'^user-map/', include('user_map.urls', namespace='user_map')),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {'template_name': 'admin/login.html'},
+        name='my_login',
+    ),
 )
